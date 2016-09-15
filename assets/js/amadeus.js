0d981d94889ca7f36c09d1e1dbe0026471d27d65
Jonathan Santos <santojon5@gmail.com>
2016-09-15 11:36:14 -0300

    update to use parametric breadcrumb

--- assets/js/amadeus.js
+++ assets/js/amadeus.js
@@ -1,6 +1,11 @@
 var Amadeus = {
+    /**
+     * Path related variables and functions
+     */
     paths: {
+        // application host
         host: window.location.host,
+        // assets path
         assets: function(path) {
             base = window.location.host + '/assets';
             if (path) {
@@ -9,6 +14,7 @@ var Amadeus = {
                 return base;
             }
         },
+        // components path
         components: function(path) {
             base = window.location.host + '/html/components';
             if (path) {
@@ -17,5 +23,19 @@ var Amadeus = {
                 return base;
             }
         }
+    },
+    /**
+     * Import HTML templates using w3data library 
+     */
+    importTemplates: function() {
+        w3IncludeHTML();
+    },
+    /**
+     * Set location in breadcrumb
+     */
+    setBreadcrumb: function(inactive, active) {
+        setTimeout(function() {
+            w3DisplayData('breadcrumb', { 'active': active, inactive: inactive });
+        }, 500);
     }
 };
