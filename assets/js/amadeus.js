05e509716e63aebfdb2eb58638ac11f1e5844d11
Jonathan Santos <santojon5@gmail.com>
2016-09-15 11:57:57 -0300

    amadeus.js fix + parametric navbar

--- assets/js/amadeus.js
+++ assets/js/amadeus.js
@@ -8,10 +8,10 @@ var Amadeus = {
      */
     paths: {
         // application host
-        host: window.location.host,
+        host: 'http://' + window.location.host,
         // assets path
         assets: function(path) {
-            base = window.location.host + '/assets';
+            base = Amadeus.paths.host + '/assets';
             if (path) {
                 return base + path;
             } else {
@@ -20,7 +20,16 @@ var Amadeus = {
         },
         // components path
         components: function(path) {
-            base = window.location.host + '/html/components';
+            base = Amadeus.paths.host + '/html/components';
+            if (path) {
+                return base + path;
+            } else {
+                return base;
+            }
+        },
+        // screens path
+        screens: function(path) {
+            base = Amadeus.paths.host + '/html/screens';
             if (path) {
                 return base + path;
             } else {
@@ -40,6 +49,18 @@ var Amadeus = {
     setBreadcrumb: function(inactive, active) {
         setTimeout(function() {
             w3DisplayData('breadcrumb', { 'active': active, inactive: inactive });
+        }, 500);
+    },
+    /**
+     * Set Up the system navbar
+     */
+    setUpNavbar: function() {
+        setTimeout(function() {
+            w3DisplayData('navbar', {
+                hostPath: Amadeus.paths.host,
+                assetPath: Amadeus.paths.assets(),
+                screenPath: Amadeus.paths.screens()
+            });
         }, 500);
     }
 };
