32c8e8f5ec399ec2d539446f4fee19658e8974c0
juliotoscano <juliotoscanodebrito@gmail.com>
2016-09-22 15:59:06 -0300

    accordion jquery #59

--- assets/js/controllers/home_subject_teacher.js
+++ assets/js/controllers/home_subject_teacher.js
@@ -1,3 +1,20 @@
 $("#datepicker").datepicker();
 $("#datepicker2").datepicker();
+ $( function() {
+        $( "#accordion" )
+          .accordion({
+            header: "> div > h3"
+          })
+          .sortable({
+            axis: "y",
+            handle: "h3",
+            stop: function( event, ui ) {
+              // IE doesn't register the blur when sorting
+              // so trigger focusout handlers to remove .ui-state-focus
+              ui.item.children( "h3" ).triggerHandler( "focusout" );
 
+              // Refresh accordion to handle new order
+              $( this ).accordion( "refresh" );
+                }
+              });
+          } );
