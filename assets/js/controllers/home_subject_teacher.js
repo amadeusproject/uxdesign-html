832cd5937aa0a532f8b81cb06b5ea4b4c1cf2006
juliotoscano <juliotoscanodebrito@gmail.com>
2016-09-26 10:28:17 -0300

    Accordion with sortable #59

--- assets/js/controllers/home_subject_teacher.js
+++ assets/js/controllers/home_subject_teacher.js
@@ -1,14 +1,14 @@
 $("#accordion")
   .accordion({
-    header: "> div > h3"
+    header: "> div > div"
   })
   .sortable({
     axis: "y",
-    handle: "h3",
+    handle: ".panel-heading",
     stop: function (event, ui) {
       // IE doesn't register the blur when sorting
       // so trigger focusout handlers to remove .ui-state-focus
-      ui.item.children("h3").triggerHandler("focusout");
+      ui.item.children(".panel-heading").triggerHandler("focusout");
 
       // Refresh accordion to handle new order
       $(this).accordion("refresh");
