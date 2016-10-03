dc0129a4b7e189fa56ab903247d77bd800941ee7
juliotoscano <juliotoscanodebrito@gmail.com>
2016-10-03 11:15:24 -0300

    create edit_end js

--- assets/js/controllers/topic_editation_presentation.js
+++ assets/js/controllers/topic_editation_presentation.js
@@ -2,3 +2,7 @@ $(".edit_card").on('click', function() {
     $(".presentation").css('display','none');
     $(".editation").css('display','block');
 })
+$(".edit_card_end").on('click', function() {
+    $(".editation").css('display','none');
+    $(".presentation").css('display','block');
+})
